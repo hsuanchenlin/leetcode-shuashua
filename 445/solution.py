4a9cd13f304cd43b8af42820d0900a654e76cb6c
hsuan-chen lin <hsuanchen@outlook.com>
2019-03-19 23:54:16 +0800

    '445'

---
+++ 445/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        x1 = 0
+        while l1:
+            x1 = x1*10+l1.val
+            l1=l1.next
+            
+        x2 = 0
+        while l2:
+            x2 = x2*10+l2.val
+            l2=l2.next
+        x = x1+x2
+        head = ListNode(0)
+        if x == 0: 
+            return head
+        while x:
+            v, x = x%10, x//10
+            head.next, head.next.next = ListNode(v), head.next
+        return head.next    
+        
