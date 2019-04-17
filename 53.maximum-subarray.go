903644269ae0a64556094e89f1f4174e39875326
hsuanchenlin <hsuanchen@outlook.com>
2019-04-17 17:31:37 +0800

    53 done in go

---
+++ 53.maximum-subarray.go
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=53 lang=golang
+ *
+ * [53] Maximum Subarray
+ *
+ * https://leetcode.com/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Easy (43.24%)
+ * Total Accepted:    502.2K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * Given an integer array nums, find the contiguous subarray (containing at
+ * least one number) which has the largest sum and return its sum.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * 
+ * 
+ * Follow up:
+ * 
+ * If you have figured out the O(n) solution, try coding another solution using
+ * the divide and conquer approach, which is more subtle.
+ * 
+ */
+func maxSubArray(nums []int) int {
+	acc, ans := -1<<31, -1<<31
+	for _, v := range nums{
+		acc = max(v, acc+v)
+		ans = max(acc, ans)
+	}
+	return ans
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
